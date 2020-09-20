ace5d644de03967669fca1db7e13060269f91357
leejaeyun95 <leejaeyun95@naver.com>
2020-09-20 19:53:57 +0900

    조건문

---
+++ 파이썬_기초핵심과정/section05-1.py
@@ -0,0 +1,31 @@
+# Section05-1
+# 파이썬 흐름제어(제어문)
+# 조건문 실습
+
+print(type(True))
+print(type(False))
+
+a = 10
+b = 0
+
+if a == b:
+    print("True")
+else:
+    print("False")
+
+if a != b:
+    print("True")
+else:
+    print("False")
+
+if a is b:
+    print("True")
+else:
+    print("False")
+
+if a is not b:
+    print("True")
+else:
+    print("False")
+
+
