c7f4f55ac26cc0f6f0a2be44c31201321cfc587f
leejaeyun95 <leejaeyun95@naver.com>
2020-10-20 00:50:06 +0900

    view 로직 처리

--- DjangoLv1/fc_community/fcuser/views.py
+++ DjangoLv1/fc_community/fcuser/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Test
+from django.http.response import HttpResponse
 
 
 # Create your views here.
@@ -7,16 +8,23 @@ def register(request):     # 나중에 url을 지정해줄건데, 해당 url로 
     if request.method =="GET":
         return render(request, 'register.htm')
     elif request.method =="POST":
-        username = request.POST['username']
+        username = request.POST['username'] # register 페이지에서 응답이 온다. 그게 GET이거나 POST이다.
         password = request.POST['password']
         re_password = request.POST['re-password']
 
-        fcuser = Test(
-            username=username,
-            password=password
-        )
-        fcuser.save()
-        return render(request, 'register.htm')
+        res_data = {}
+        if password is not re_password:
+            res_data['error'] = "비밀번호가 다릅니다!"
+             
+        else:
+            res_data['error'] = ''
+            fcuser = Test(  # 그러면 모델에서 Test 테이블을 가져와
+                username=username,  # username과
+                password=password   # password를 저장한다.
+            )
+            fcuser.save()   # 생성된 값은 저장한다.
 
-    return render(request, 'register.htm')  # 반환하고 싶은 파일 입력, 경로는 templates를 자동으로 바라보고 있으므로 생략
-                                            # 만약 templates안에 하위 폴더가 존재한다면, folder/folder2/register.html 등으로 표기!
+        return render(request, 'register.htm', res_data)  # 다시 register 화면을 render한다(띄운다) (, , view에서 사용된 인자 전달)
+
+        # 반환하고 싶은 파일 입력, 경로는 templates를 자동으로 바라보고 있으므로 생략
+        # 만약 templates안에 하위 폴더가 존재한다면, folder/folder2/register.html 등으로 표기!
